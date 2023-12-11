cbedd1c8ea488c4d16516a1d3285ad6eab7cb2a3
Sammy Ferreira <sammyfe2021@gmail.com>
2023-12-11 11:56:54 -0300

    :ok_hand: style: icons products infors

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -10,9 +10,7 @@ export default function MaxWidthWrapper({
   children,
 }: MaxWidthWrapperProps) {
   return (
-    <div
-      className={(cn('mx-auto w-full max-w-7xl px-2.5 md:px-20'), className)}
-    >
+    <div className={cn('mx-auto w-full max-w-7xl px-2.5 md:px-20', className)}>
       {children}
     </div>
   )
